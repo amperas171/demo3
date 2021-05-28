5dcd394f9fee1f5c963e82a5bf0cb5c40cb0a862
amperas17 <amperas17@gmail.com>
2021-05-29 01:25:35 +0300

    rewrite editSubtask 5

--- src/main/java/com/amperas17/demo3/users/data/subtask/SubtaskRepository.java
+++ src/main/java/com/amperas17/demo3/users/data/subtask/SubtaskRepository.java
@@ -3,12 +3,14 @@ package com.amperas17.demo3.users.data.subtask;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface SubtaskRepository extends CrudRepository<SubtaskEntity, Integer> {
 
     @Query("select u from SubtaskEntity u where u.id = ?1")
     SubtaskEntity findByID(int id);
 
+    @Transactional
     @Modifying
     @Query("update SubtaskEntity u set u.name = ?2 where u.id = ?1")
     void updateNameByID(int id, String name);
