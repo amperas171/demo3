da3431e6865830e2337750f65c85a9e55aec488e
amperas17 <amperas17@gmail.com>
2021-05-30 23:34:38 +0300

    comment JsonIdentityInfo

--- src/main/java/com/amperas17/demo3/users/data/user/UserCredsEntity.java
+++ src/main/java/com/amperas17/demo3/users/data/user/UserCredsEntity.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "users")
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+//@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class UserCredsEntity {
 
     //@Id // Сообщяем ORM что это поле - Primary Key
