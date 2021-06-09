9fcacf9d0f9f1fe988aa677063effd3dd1f16843
dudchenkovv <dudchenkovv@psbank.ru>
2021-06-09 13:13:17 +0300

    getAllExceptCurrentByQuery 0

--- src/main/java/com/amperas17/demo3/users/data/user/UserRepository.java
+++ src/main/java/com/amperas17/demo3/users/data/user/UserRepository.java
@@ -19,11 +19,11 @@ public interface UserRepository extends CrudRepository<UserCredsEntity, Integer>
     @Query("select u from UserCredsEntity u where u.id != ?1")
     List<UserCredsEntity> getAllExceptCurrent(int id);
 
-    //@Query("select u from UserCredsEntity u where u.id != ?1 AND position(?2 in u.name)>0")
-    //List<UserCredsEntity> getAllExceptCurrentByQuery(int id, String query);
-
-    @Query("select u from UserCredsEntity u where u.id != ?1 AND to_tsvecor(u.name) @@ plainto_tsquery(query)")
+    @Query("select u from UserCredsEntity u where u.id != ?1 AND position(?2 in u.name)>0")
     List<UserCredsEntity> getAllExceptCurrentByQuery(int id, String query);
+
+    //@Query("select u from UserCredsEntity u where u.id != ?1 AND to_tsvecor(u.name) @@ plainto_tsquery(query)")
+    //List<UserCredsEntity> getAllExceptCurrentByQuery(int id, String query);
 
     @Transactional
     @Modifying
