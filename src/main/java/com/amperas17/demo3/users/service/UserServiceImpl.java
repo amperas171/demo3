1e0fdcb63f1f208c658e7716d55be1da919682d2
dudchenkovv <dudchenkovv@psbank.ru>
2021-06-09 13:42:21 +0300

    getAllExceptCurrentByQuery 7

--- src/main/java/com/amperas17/demo3/users/service/UserServiceImpl.java
+++ src/main/java/com/amperas17/demo3/users/service/UserServiceImpl.java
@@ -63,7 +63,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> getAllExceptCurrentByQuery(int id, String query) {
         List<User> users = new ArrayList<>();
-        for (UserCredsEntity uce : userRepository.getAllExceptCurrentByQuery(id, query)) {
+        for (UserCredsEntity uce : userRepository.getAllExceptCurrentByQuery(id, "%"+query+"%")) {
             users.add(new User(uce.getId(), uce.getName(), uce.getSurname(), uce.getEmail(), uce.getPhone()));
         }
         return users;
